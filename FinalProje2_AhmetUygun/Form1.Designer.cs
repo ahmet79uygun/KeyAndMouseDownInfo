bb2b2d5fe9e2312cb8882497200608459e352c7c
ahmet uygun <ahmetuygun140@gmail.com>
2021-09-02 18:35:38 +0300

    Add project files.

---
+++ FinalProje2_AhmetUygun/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace FinalProje2_AhmetUygun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myKeyAndMouseDownInfo1 = new FinalProje2_AhmetUygun.MyKeyAndMouseDownInfo();
+            this.SuspendLayout();
+            // 
+            // myKeyAndMouseDownInfo1
+            // 
+            this.myKeyAndMouseDownInfo1.Location = new System.Drawing.Point(0, 0);
+            this.myKeyAndMouseDownInfo1.Name = "myKeyAndMouseDownInfo1";
+            this.myKeyAndMouseDownInfo1.Size = new System.Drawing.Size(503, 255);
+            this.myKeyAndMouseDownInfo1.TabIndex = 0;
+            this.myKeyAndMouseDownInfo1.Text = "myKeyAndMouseDownInfo1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 240);
+            this.Controls.Add(this.myKeyAndMouseDownInfo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyKeyAndMouseDownInfo myKeyAndMouseDownInfo1;
+    }
+}
+
---
+++ FinalProje2_AhmetUygun/MyKeyAndMouseDownInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinalProje2_AhmetUygun
+{
+    public partial class MyKeyAndMouseDownInfo : Control
+    {
+        Label lbl = new Label();
+
+        public MyKeyAndMouseDownInfo()
+        {
+            InitializeComponent();
+            lbl.AutoSize = true;
+            lbl.Top = 10;
+            lbl.Left = 10;
+            lbl.BackColor = Color.BurlyWood;
+            lbl.Font = new Font("Arial", 20);
+            lbl.ForeColor = Color.Green;
+            lbl.Text = "herhangi bir tuşa basın";
+            this.Controls.Add(lbl);
+            KeyPress += new KeyPressEventHandler(keypressed);
+            MouseUp += new MouseEventHandler(Form1_MouseDown);
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+        private void keypressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                lbl.Text = "Enter";
+            else if (e.KeyChar == (char)Keys.Escape)
+                lbl.Text = "Escape";
+            else if (e.KeyChar == (char)Keys.Space)
+                lbl.Text = "Space";
+            else // normal tuşlar...(qwert..)
+                lbl.Text = e.KeyChar.ToString();
+        }
+
+
+
+        void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+                lbl.Text = "Mouse Right";
+            else if (e.Button == MouseButtons.Left)
+                lbl.Text = "Mouse Left";
+            else if (e.Button == MouseButtons.Middle)
+                lbl.Text = "Mouse Middle";
+            else
+                lbl.Text = "not defined";
+        }
+    }
+}
